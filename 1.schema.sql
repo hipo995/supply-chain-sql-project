d7ba56e1c8bc958bf3bbe5f87fb86c5d81cae070
hipo995 <hibalakhdioui9@gmail.com>
2026-05-31 12:00:27 +0200

    Update 1.schema.sql

--- 1.schema.sql
+++ 1.schema.sql
@@ -1,3 +1,6 @@
+
+Fact Tables
+
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     name VARCHAR(100),
@@ -24,3 +27,44 @@ CREATE TABLE order_status_history (
     status VARCHAR(50),
     status_date DATE
 );
+
+Dimension Tables
+
+1. dim_customer (erste Dimension)
+
+CREATE TABLE dim_customer (
+    customer_key INT PRIMARY KEY,
+    customer_id INT,
+    name VARCHAR(100),
+    country VARCHAR(50),
+    city VARCHAR(50),
+    segment VARCHAR(10),
+    email VARCHAR(100),
+    has_phone INT,
+    signup_date DATE
+);
+
+2. dim_date
+CREATE TABLE dim_date (
+    date_key DATE PRIMARY KEY,
+    year INT,
+    month INT,
+    day INT,
+    weekday VARCHAR(10),
+    quarter INT
+);
+3. dim_shipment
+CREATE TABLE dim_shipment (
+    shipment_key INT PRIMARY KEY,
+    shipped_date DATE,
+    delivery_date DATE,
+    delivery_days INT
+);
+4. dim_order_status
+CREATE TABLE dim_order_status (
+    status_key INT PRIMARY KEY,
+    status VARCHAR(50)
+);
+
+
+
